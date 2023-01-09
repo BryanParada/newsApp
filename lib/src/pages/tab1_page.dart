498d3e3fe0409f66ef209275371f1a57bcd492f7
Bryan Parada <bryan.parada.cea@gmail.com>
2023-01-09 15:30:58 -0300

    F294 - Creating widget to display news

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/src/services/news_service.dart';
+import 'package:news_app/src/widgets/list_news.dart';
 import 'package:provider/provider.dart';
 
 class Tab1Page extends StatelessWidget {
@@ -7,14 +8,12 @@ class Tab1Page extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final newsService = Provider.of<NewsService>(context);
-
+    final headlines = Provider.of<NewsService>(context).headlines;
+    //newsService.headlines;
 
 
     return Scaffold(
-      body: Center(
-         child: Text('Tab1Page'),
-      ),
+      body: ListNews(headlines ) 
     );
   }
 }
